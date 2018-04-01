d02938ee95307087d3ec55ef5393e825c8146125
abbasashraf <abbas.ashraf19@gmail.com>
2018-04-01 08:08:36 -0700

    mongo client / mongoDB

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -1,20 +1,68 @@
-const MongoClient = require('mongodb').MongoClient
+// const MongoClient = require('mongodb').MongoClient
+const { MongoClient, ObjectId } = require('mongodb');
+
 
 // after version 3.0 we use client object not db
 MongoClient.connect('mongodb://localhost:27017/animals', (err, client) => {
     if (err) throw err
 
-    const db = client.db('animals');
-    db.collection('mamals').insertOne({
-        name: 'dog',
-        legs: '4'
-    }, (err, result) => {
-        if (err) {
-            return console.log(err)
-        }
-    })
+    // console.log(ObjectId())
+    // console.log('CONNECTED')
 
-    console.log('INSERTED')
+    // const object = new ObjectId()
+
+    const db = client.db('animals');
+
+    // ========== CREATED DATA ===========
+    // db.collection('mamals').insertOne({
+    //     name: 'dog',
+    //     legs: '4'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+    // })
+
+    // console.log('INSERTED')
+
+
+
+    // =========== FETCHING DATA ==============
+    // db.collection('mamals').find().toArray((err, result) => {
+    //     if (err) throw err;
+    //     console.log(result)
+    // })
+
+    // ============ UPDATING ===============
+    // db.collection('mamals').findOneAndUpdate({
+    //     _id: new ObjectId('5abf721197df8823f4e7aac6')
+    // }, {
+    //         $set: {
+    //             name: "updated"
+    //         }
+    //     })
+    //     .then(
+    //     (result) => {
+    //         console.log(result)
+    //     }
+    //     ).catch(
+    //     (err) => {
+    //         console.log(err)
+
+    //     })
+    // console.log('UPDATED')
+
+    // =============== DELETE ============
+
+    // db.collection('mamals').deleteMany({ name: 'abbas'});
+    // db.collection('mamals').deleteOne({ name: 'abbas'});
+    // db.collection('mamals').deleteAndFind({ name: 'abbas'});
+
+    db.collection('mamals').findOneAndDelete({
+        _id: new ObjectId('5abf721197df8823f4e7aac6')
+    }).then(result => {
+        console.log(result)
+    })
 
 
 })
